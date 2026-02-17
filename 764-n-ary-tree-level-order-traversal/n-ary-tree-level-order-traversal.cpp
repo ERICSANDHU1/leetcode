8f6703c961ccc34132c81c656280c237bd494d9d
Eric Sandhu <sandhue098@gmail.com>
2026-02-17 20:30:34 +0530

    Time: 7 ms (95.66%) | Memory: 16.1 MB (47.34%) - LeetSync

--- 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.cpp
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.cpp
@@ -20,25 +20,25 @@ public:
 
 class Solution {
 public:
- void dfs(Node* root , vector<vector<int>> &ans){
-    if(root == NULL) return ;
+void dfs(Node* root , vector<vector<int>> &ans){
+    if(root == NULL) return;
     queue<Node*>q;
     q.push(root);
-
     while(!q.empty()){
-         int n = q.size();
-            vector<int>level;
-         for(int i = 0 ; i < n ; i++){
-             root = q.front();
-             q.pop();
-             level.push_back(root->val);
-             for(int j = 0 ; j < root->children.size();j++){
-             if(root->children[j]) q.push(root->children[j]);
-            }
+        int n = q.size();
+        vector<int>level;
+        for(int i = 0 ; i < n ; i ++){
+        root = q.front();
+        q.pop();
+
+         level.push_back(root->val);
+         for(int j =  0 ; j < root->children.size();j++){
+            if(root->children[j]) q.push(root->children[j]);
+          }
         }
         ans.push_back(level);
     }
- }
+}
     vector<vector<int>> levelOrder(Node* root) {
         vector<vector<int>> ans;
         dfs(root,ans);
