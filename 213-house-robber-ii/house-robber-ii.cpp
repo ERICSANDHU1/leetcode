ab4bb2793e05c471d610f37eb5f2bf94228dec0e
Eric Sandhu <sandhue098@gmail.com>
2026-02-25 10:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (38.10%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,32 +2,28 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        
-        if (n == 1) return nums[0];
-        if (n == 2) return max(nums[0], nums[1]);
+        // if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0],nums[1]);
 
-        // Case 1: Rob from 0 → n-2
-        vector<int> dp1(n);
+        vector<int>dp1(n);
         dp1[0] = nums[0];
-        dp1[1] = max(nums[0], nums[1]);
+        dp1[1] = max(nums[0],nums[1]);
 
-        for (int i = 2; i < n - 1; i++) {
-            dp1[i] = max(dp1[i - 1], nums[i] + dp1[i - 2]);
-        }
+        for(int i = 2 ; i < n-1; i++){
+            dp1[i] = max(dp1[i-1],nums[i]+dp1[i-2]);
+        } 
+        int case1 = dp1[n-2];
 
-        int case1 = dp1[n - 2];
-
-        // Case 2: Rob from 1 → n-1
-        vector<int> dp2(n);
+        vector<int>dp2(n);
         dp2[1] = nums[1];
-        dp2[2] = max(nums[1], nums[2]);
+        dp2[2] = max(nums[1],nums[2]);
 
-        for (int i = 3; i < n; i++) {
-            dp2[i] = max(dp2[i - 1], nums[i] + dp2[i - 2]);
+        for(int i = 3 ; i < n ; i ++){
+            dp2[i] = max(dp2[i-1],nums[i] + dp2[i-2]);
         }
+        int case2 = dp2[n-1];
 
-        int case2 = dp2[n - 1];
-
-        return max(case1, case2);
+        return max(case1,case2);
     }
 };
