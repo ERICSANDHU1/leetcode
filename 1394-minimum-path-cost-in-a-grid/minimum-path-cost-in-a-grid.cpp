c95187e393f1429fcbafda000503228e184921c1
Eric Sandhu <sandhue098@gmail.com>
2026-03-01 15:56:52 +0530

    Time: 16 ms (62.31%) | Memory: 84.7 MB (59.69%) - LeetSync

---
+++ 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+
+        // Initialize first row
+        for (int j = 0; j < m; j++)
+            dp[0][j] = grid[0][j];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {          // previous column
+                for (int k = 0; k < m; k++) {      // current column
+                    dp[i][k] = min(dp[i][k],
+                        dp[i-1][j] 
+                        + moveCost[grid[i-1][j]][k] 
+                        + grid[i][k]);
+                }
+            }
+        }
+
+        return *min_element(dp[n-1].begin(), dp[n-1].end());
+    }
+};
