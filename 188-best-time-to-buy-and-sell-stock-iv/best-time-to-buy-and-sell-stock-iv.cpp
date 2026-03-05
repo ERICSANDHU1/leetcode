a5140e1d529dee98a1317f681d9051f693c33dc4
Eric Sandhu <sandhue098@gmail.com>
2026-03-06 00:26:09 +0530

    Time: 9 ms (47.60%) | Memory: 18.3 MB (39.10%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-
-        vector<vector<vector<int>>> ERIClovesVANSHIKA(
-            n+1, vector<vector<int>>(2, vector<int>(k+1,0))
-        );
-
+        vector<vector<vector<int>>> ERIClovesVANSHIKA(n+1, vector<vector<int>>(2, vector<int>(k+1,0)));
         for(int i = n-1 ; i >= 0 ; i--){
             for(int buy = 0 ; buy <= 1 ; buy++){
                 for(int cap = 1 ; cap <= k ; cap++){
-
                     if(buy){
                         ERIClovesVANSHIKA[i][buy][cap] =
                         max(-prices[i] + ERIClovesVANSHIKA[i+1][0][cap],
@@ -25,7 +20,6 @@ public:
                 }
             }
         }
-
         return ERIClovesVANSHIKA[0][1][k];
     }
 };
